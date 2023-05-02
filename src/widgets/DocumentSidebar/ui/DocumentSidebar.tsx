f42f6cf8b40db0e0967c7d12c0db5b30bb3113b7
Nikita Kisly <sour945@gmail.com>
2023-05-02 22:03:12 +0300

    feat: add cache for introspection request

--- src/widgets/DocumentSidebar/ui/DocumentSidebar.tsx
+++ src/widgets/DocumentSidebar/ui/DocumentSidebar.tsx
@@ -1,13 +1,22 @@
 import { FC } from 'react';
 import { DocumentBreadCrumbs, DocumentTypeHeader, DocumentArgs, DocumentFields, DocumentRoot } from 'features';
 import { useAppSelector, useIntrospection, useTypesInfo } from 'shared';
+// import { GraphQLObjectType, OperationTypeNode } from 'graphql';
 
 const DocumentSideBar: FC = () => {
   const { introspection } = useIntrospection();
   const { breadCrumbs, currentTypeName } = useAppSelector((state) => state.breadCrumbsReducer);
   const { fieldInfo, thisType } = useTypesInfo(introspection, breadCrumbs, currentTypeName);
 
-  // console.log(introspection);
+  // useEffect(() => {
+  //   if (schema) {
+  //     const a = schema.current?.getRootType(OperationTypeNode.QUERY);
+  //     const b = schema.current?.getType('FilmsConnection');
+  //     if (b && 'getFields' in b) {
+  //       console.log(b.getFields());
+  //     }
+  //   }
+  // }, [schema]);
 
   return (
     <section>
