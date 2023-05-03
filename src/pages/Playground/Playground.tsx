7ac1885dc62c85391b291ea1c7394ffec28334cc
Nikita Kisly <sour945@gmail.com>
2023-05-03 17:27:22 +0300

    feat: update: navigation

--- src/pages/Playground/Playground.tsx
+++ src/pages/Playground/Playground.tsx
@@ -1,27 +1,24 @@
-import { FC, lazy, Suspense, useEffect, useState } from 'react';
+import { FC, lazy, Suspense } from 'react';
 import CircularProgress from '@mui/material/CircularProgress';
 import { QueryConfigBar, QueryTerminal, ResponseBar } from 'widgets';
 import { Grid } from '@mui/material';
+import { useAppSelector } from 'shared';
 
 const DocumentationSideBar = lazy(() => import('widgets'));
 
 export const PlayGround: FC = () => {
-  const [isOpenSideBar, setIsOpenSideBar] = useState(false);
-
-  useEffect(() => {
-    setIsOpenSideBar(true);
-  }, []);
+  const { isDocumentOpen } = useAppSelector((state) => state.documentReducer);
 
   return (
     <Grid container sx={{ height: '100%' }}>
-      <Grid xl={4} lg={4} item={true}>
-        {isOpenSideBar && (
+      {isDocumentOpen && (
+        <Grid xl={4} lg={4} item={true}>
           <Suspense fallback={<CircularProgress />}>
             <DocumentationSideBar />
           </Suspense>
-        )}
-      </Grid>
-      <Grid xl={4} lg={4} item={true}>
+        </Grid>
+      )}
+      <Grid xl={isDocumentOpen ? 4 : 8} lg={isDocumentOpen ? 4 : 8} item={true}>
         <Grid>
           <QueryTerminal />
         </Grid>
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,16 +2,23 @@ import { type FC } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 import { Box, ListItemIcon, ListItemButton, ListItem, List, Divider } from '@mui/material';
 import { Container } from './Container.styled';
+import { useAppActions } from 'shared';
 import { NAVIGATION } from '../constants';
 
 export const Navbar: FC = () => {
   const navigate = useNavigate();
+  const { setIsDocumentOpen } = useAppActions();
+
+  const routeHandler = (nav: (typeof NAVIGATION)[0]) => {
+    navigate(nav.route);
+    if (nav.name === 'docs') setIsDocumentOpen();
+  };
 
   return (
     <Container>
       <List>
         {NAVIGATION.map((nav) => (
-          <ListItem key={nav.id} disablePadding onClick={() => navigate(nav.route)}>
+          <ListItem key={nav.id} disablePadding onClick={() => routeHandler(nav)}>
             <ListItemButton>
               <ListItemIcon
                 sx={{
