376237991e4c08e9790bd59893b7b975cce55a21
mshns <mishanos412@gmail.com>
2023-05-04 00:28:07 +0300

    feat: add theme provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
+import { ThemeProvider } from '@emotion/react';
 
 import { App } from 'app';
 
+import { theme } from 'shared';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -1,6 +1,25 @@
 import { type FC } from 'react';
 import AppBar from '@mui/material/AppBar';
+import { Box, Button, IconButton, Stack, Toolbar, Typography } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 
 export const Header: FC = () => {
-  return <AppBar position="sticky">Header</AppBar>;
+  return (
+    <Box sx={{ flexGrow: 1, backgroundColor: 'primary.dark' }}>
+      <AppBar position="sticky">
+        <Toolbar>
+          <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            GraphiQL
+          </Typography>
+          <Stack spacing={1} direction="row">
+            <Button variant="contained">Sign In</Button>
+            <Button variant="contained">Sign Up</Button>
+          </Stack>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 };
