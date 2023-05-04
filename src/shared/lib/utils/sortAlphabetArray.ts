6e0bae21e058759377206a37092671eff369aed4
Nikita Kisly <sour945@gmail.com>
2023-05-04 21:45:11 +0300

    fix: fix sort util

--- src/shared/lib/utils/sortAlphabetArray.ts
+++ src/shared/lib/utils/sortAlphabetArray.ts
@@ -1,5 +1,5 @@
 import { IntrospectionField, IntrospectionInputValue } from 'graphql';
 
 export const sortAlphabetArray = <T extends IntrospectionInputValue | IntrospectionField>(array: readonly T[]) => {
-  return array.slice(0).sort((a, b) => a.name.localeCompare(b.name));
+  return array.slice().sort((a, b) => a.name.localeCompare(b.name));
 };
