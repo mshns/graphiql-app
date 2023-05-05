36c225d7aed526a4be7336163cd8c079405562ae
Nikita Kisly <sour945@gmail.com>
2023-05-05 09:53:33 +0300

    refactor: remove unused files packages and code

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import path from 'path';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vite';
@@ -8,10 +7,5 @@ export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   build: {
     sourcemap: true
-  },
-  resolve: {
-    alias: {
-      '~variables': path.resolve(__dirname, 'src/app/styles/variables.scss')
-    }
   }
 });
