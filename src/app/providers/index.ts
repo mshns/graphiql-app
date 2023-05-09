6b26563f38993106416324ff4e74969e247eb9ca
Nikita Kisly <sour945@gmail.com>
2023-05-09 15:27:28 +0300

    refactor: remove apollo client

--- src/app/providers/index.ts
+++ src/app/providers/index.ts
@@ -1,7 +1,6 @@
 import compose from 'compose-function';
 
-import { withApollo } from './with-apollo';
 import { withMUI } from './with-mui';
 import { withStore } from './with-store';
 
-export const withProviders = compose(withApollo, withMUI, withStore);
+export const withProviders = compose(withMUI, withStore);
