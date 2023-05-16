159cbfdd50cc8ef2500cb7184d788eb3d62356c9
mshns <mishanos412@gmail.com>
2023-05-16 16:01:41 +0300

    feat: add language setting in local storage

--- src/features/HeaderSettings/ui/HeaderSettings.tsx
+++ src/features/HeaderSettings/ui/HeaderSettings.tsx
@@ -6,6 +6,11 @@ import { Box, Button, Divider, Typography } from '@mui/material';
 export const HeaderSettings: FC = () => {
   const { i18n, t } = useTranslation(['layout']);
 
+  const handleChangeLanguage = (language: string) => {
+    i18n.changeLanguage(language);
+    localStorage.setItem('language', language);
+  };
+
   return (
     <Box sx={{ width: 250, p: 2, display: 'flex', flexDirection: 'column' }}>
       <Typography sx={{ fontSize: 24, color: 'secondary.main' }}>{t('Settings')}</Typography>
@@ -13,10 +18,10 @@ export const HeaderSettings: FC = () => {
 
       <Typography sx={{ fontSize: 20, color: 'secondary.main' }}>{t('Language')}</Typography>
       <Divider sx={{ mb: 1 }} />
-      <Button variant="contained" sx={{ mt: 1 }} onClick={() => i18n.changeLanguage('en')}>
+      <Button variant="contained" sx={{ mt: 1 }} onClick={() => handleChangeLanguage('en')}>
         {t('English')}
       </Button>
-      <Button variant="contained" sx={{ mt: 1 }} onClick={() => i18n.changeLanguage('ru')}>
+      <Button variant="contained" sx={{ mt: 1 }} onClick={() => handleChangeLanguage('ru')}>
         {t('Russian')}
       </Button>
       <Divider sx={{ m: 3 }} />
