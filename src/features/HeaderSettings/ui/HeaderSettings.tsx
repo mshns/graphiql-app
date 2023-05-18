5ec14c4019b69132cf284109fbd2c8b31688b617
mshns <mishanos412@gmail.com>
2023-05-18 06:49:05 +0300

    fix: change buttons for switch language and mode in settings

--- src/features/HeaderSettings/ui/HeaderSettings.tsx
+++ src/features/HeaderSettings/ui/HeaderSettings.tsx
@@ -1,18 +1,21 @@
 import { FC, useContext } from 'react';
 import { useTranslation } from 'react-i18next';
 
-import { Box, Button, Divider, IconButton, Typography } from '@mui/material';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
+import { Box, Button, Divider, Typography } from '@mui/material';
 
 import { ColorModeContext } from 'app';
 
 export const HeaderSettings: FC = () => {
   const { i18n, t } = useTranslation(['layout']);
 
-  const handleChangeLanguage = (language: string) => {
-    i18n.changeLanguage(language);
-    localStorage.setItem('language', language);
+  const handleChangeLanguage = () => {
+    if (i18n.language === 'en') {
+      i18n.changeLanguage('ru');
+      localStorage.setItem('language', 'ru');
+    } else {
+      i18n.changeLanguage('en');
+      localStorage.setItem('language', 'en');
+    }
   };
 
   const { mode, toggleColorMode } = useContext(ColorModeContext);
@@ -21,42 +24,16 @@ export const HeaderSettings: FC = () => {
     <Box sx={{ width: 250, p: 2, display: 'flex', flexDirection: 'column' }}>
       <Typography sx={{ fontSize: 24, color: 'secondary.main' }}>{t('Settings')}</Typography>
       <Divider sx={{ mb: 3 }} />
-
       <Typography sx={{ fontSize: 20, color: 'secondary.main' }}>{t('Language')}</Typography>
       <Divider sx={{ mb: 1 }} />
-      <Button variant="contained" sx={{ mt: 1 }} onClick={() => handleChangeLanguage('en')}>
-        {t('English')}
-      </Button>
-      <Button variant="contained" sx={{ mt: 1 }} onClick={() => handleChangeLanguage('ru')}>
-        {t('Russian')}
+      <Button variant="contained" sx={{ mt: 1 }} onClick={handleChangeLanguage}>
+        {i18n.language === 'en' ? t('Russian') : t('English')}
       </Button>
       <Divider sx={{ m: 3 }} />
-
       <Typography sx={{ fontSize: 20, color: 'secondary.main' }}>{t('Mode')}</Typography>
       <Divider sx={{ mb: 1 }} />
       <Button variant="contained" sx={{ mt: 1 }} onClick={toggleColorMode}>
-        {t('Light')}
-      </Button>
-
-      <Box
-        sx={{
-          display: 'flex',
-          width: '100%',
-          alignItems: 'center',
-          justifyContent: 'center',
-          bgcolor: 'background.default',
-          color: 'text.primary',
-          borderRadius: 1,
-          p: 3
-        }}
-      >
-        {mode} mode
-        <IconButton sx={{ ml: 1 }} onClick={toggleColorMode} color="inherit">
-          {mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
-        </IconButton>
-      </Box>
-      <Button variant="contained" sx={{ mt: 1 }}>
-        {t('Dark')}
+        {mode === 'light' ? t('Dark') : t('Light')}
       </Button>
       <Divider sx={{ m: 3 }} />
     </Box>
