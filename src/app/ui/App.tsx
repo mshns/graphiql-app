b3e7d4a5502b1b9387495de375b3d8ff8b0f6743
Nikita Kisly <sour945@gmail.com>
2023-05-22 17:35:11 +0300

    feat: add toastify

--- src/app/ui/App.tsx
+++ src/app/ui/App.tsx
@@ -1,10 +1,13 @@
+import { ToastContainer } from 'react-toastify';
 import { withProviders } from '../providers';
 import { Router } from '../router';
+import 'react-toastify/dist/ReactToastify.min.css';
 
 const App = () => {
   return (
     <div className="app">
       <Router />
+      <ToastContainer />
     </div>
   );
 };
--- src/entities/RequestButton/ui/RequestButton.tsx
+++ src/entities/RequestButton/ui/RequestButton.tsx
@@ -1,28 +1,40 @@
 import { FC } from 'react';
 import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+import { toast } from 'react-toastify';
 import { gql } from 'graphql-request';
 import { Button } from '@mui/material';
-import { isGqlValid, useAppActions, useAppSelector } from 'shared';
+import { useAppActions, useAppSelector } from 'shared';
 
 export const RequestButton: FC = () => {
-  const { query, variables } = useAppSelector((state) => state.editorReducer);
+  const { query, variables, headers } = useAppSelector((state) => state.editorReducer);
   const { setRequestObject } = useAppActions();
 
   const requesthandler = () => {
     setRequestObject(null);
-    const validatonResult = isGqlValid(query);
-
-    if (validatonResult instanceof Error) {
-      // console.log(validatonResult.message);
-      return;
-    }
 
     const queryRequest = gql`
       ${query}
     `;
-    const variablesRequest = JSON.parse(variables);
 
-    setRequestObject({ query: queryRequest, variables: variablesRequest });
+    try {
+      const variablesRequest = JSON.parse(variables);
+      const headersRequest = JSON.parse(headers);
+
+      setRequestObject({ query: queryRequest, variables: variablesRequest, headers: headersRequest });
+    } catch (error) {
+      if (error instanceof Error) {
+        toast.error(error.message, {
+          position: 'bottom-left',
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: 'light'
+        });
+      }
+    }
   };
 
   return (
